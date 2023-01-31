7866d0efac0e9332d50d3e41f1836b4a49c42bbe
Monal Kumar <72959202+Monalkumar@users.noreply.github.com>
2023-01-31 18:16:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-const heading= React.createElement("h1", {
-    id:"title"
-}, "Heading 1");
-const heading2= React.createElement("h2", {
-    id:"title",
-    hello:"hello"
-}, "Heading 2");
-const container= React.createElement("div", {
-    id:"container"
-
-}, [heading , heading2]
-);
-
-
-console.log(heading)
+const heading = React.createElement("h1", {id:"title"} ,"hello world");
+const heading1 = React.createElement("h2", {id:"hello"} ,"hello world");
+const container = React.createElement("div", {id:"container"} ,[heading, heading1]);
 const root = ReactDOM.createRoot(document.getElementById("root"))
-//passing a react element inside the root
 root.render(container)
